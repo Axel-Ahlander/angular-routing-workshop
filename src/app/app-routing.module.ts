6311675a864bec1e2f9c5a04cc867177b56b7231
Axel Ahlander <axel.ahlander@gmail.com>
2025-02-17 12:30:27 +0100

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { WatchComponent } from './watch/watch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path: 'contact',
+  component: ContactComponent
+},
+
+{
+  path: 'about-me',
+  component: AboutMeComponent
+},
+
+{
+  path: 'watch/:id',
+  component: WatchComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
